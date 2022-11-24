4b0ad0842cad18e7ea53fe402b017e0e1b8a3b8e
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-11-24 18:44:27 +0600

    done

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,9 +7,11 @@ import { signOut } from "firebase/auth";
 import { auth } from "../pages/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
-import { collection } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
 import { db } from "../pages/firebase";
 import getOtherEmail from "../utils/getOtherEmail";
+import { useRouter } from "next/router";
+
 const Sidebar = () => {
   const [user] = useAuthState(auth);
   const [snapshot, loading, error] = useCollection(collection(db, "chats"));
@@ -17,37 +19,46 @@ const Sidebar = () => {
     id: doc.id,
     ...doc.data(),
   }));
+  const router = useRouter();
+  const redirect = (id) => {
+    router.push(`/chat/${id}`);
+  };
+  const chatExists = (email) =>
+    chats?.find(
+      (chat) => chat.users.includes(user.email) && chat.users.includes(email)
+    );
+  const newChat = async () => {
+    const input = prompt("Enter email of chat required");
+    if (!chatExists(input) && input != user.email) {
+      await addDoc(collection(db, "chats"), { users: [user.email, input] });
+    }
+  };
   const chatList = () => {
-    //  return chats
-    //    ?.filter((chat) => chat.users.includes(user.email))
-    //    .map((chat) => (
-    //      <Flex
-    //        key={Math.random()}
-    //        p={3}
-    //        align="center"
-    //        _hover={{ bg: "gray.100", cursor: "pointer" }}
-    //        onClick={() => redirect(chat.id)}
-    //      >
-    //        <Avatar src="" marginEnd={3} />
-    //        <Text>{getOtherEmail(chat.users, user)}</Text>
-    //      </Flex>
-    //    ));
     return (
       <>
-        {chats?.map((chat) => (
-          <Flex
-            key={Math.random()}
-            p={3}
-            align="center"
-            _hover={{ bg: "gray.100", cursor: "pointer" }}
-            //  onClick={() => redirect(chat.id)}
-          >
-            <Avatar src="" marginEnd={3} />
-            <Text>{getOtherEmail(chat.users, user)}</Text>
-          </Flex>
-        ))}
+        {/* ekhn jader sathe amr kotha hoy na tader ke ami filter out korbo
+      eikhane theke */}
+        {chats
+          ?.filter((chat) => chat.users.includes(user.email))
+          .map((chat) => (
+            <Flex
+              key={Math.random()}
+              p={3}
+              align="center"
+              _hover={{ bg: "gray.100", cursor: "pointer" }}
+              onClick={() => redirect(chat.id)}
+            >
+              <Avatar src="" marginEnd={3} />
+              {/* chat.users er moddhe joto user ache tar moddhe theke user er 
+            email dekhabe */}
+              {/* chat.users er moddhe onk user ache jader sathe amr
+              chat hoy nai.. jader sathe hoice sudhu tader ke dekhabe */}
+              <Text>{getOtherEmail(chat.users, user)}</Text>
+            </Flex>
+          ))}
       </>
     );
+  };
   console.log(chats);
   return (
     <>
@@ -83,7 +94,7 @@ const Sidebar = () => {
         </Flex>
 
         {/* onClick={() => newChat()} */}
-        <Button m={5} p={4}>
+        <Button m={5} p={4} onClick={() => newChat()}>
           New Chat
         </Button>
         <Flex
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,25 +1,109 @@
-import { Avatar, Button, Flex, Heading, Input, Text } from "@chakra-ui/react";
-import React from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+import {
+  Avatar,
+  Button,
+  Flex,
+  FormControl,
+  Heading,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import React, { useEffect, useRef } from "react";
 import Sidebar from "../../components/Sidebar";
+import { useRouter } from "next/router";
+import {
+  useCollectionData,
+  useDocumentData,
+} from "react-firebase-hooks/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import getOtherEmail from "../../utils/getOtherEmail";
+import { useState } from "react";
 
 const chatBox = () => {
-  const Topbar = () => {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const { id } = router.query;
+  const q = query(
+    collection(db, "chats", id, "messages"),
+    orderBy("timestamp")
+  );
+  const [messages] = useCollectionData(q);
+  console.log({ messages });
+  const [chat] = useDocumentData(doc(db, "chats", id));
+  console.log(chat);
+  const bottomChat = useRef();
+  const getMessages = () => {
+    messages?.map((msg) => {
+      const sender = msg.sender === user.email;
+      return (
+        <Flex
+          key={Math.random()}
+          bg={sender ? "blue.100" : "green.100"}
+          w="fit-content"
+          minWidth="100px"
+          borderRadius="lg"
+          p={3}
+          m={1}
+          alignSelf={sender ? "flex-start" : "flex-end"}
+        >
+          <Text>{msg.text}</Text>
+        </Flex>
+      );
+    });
+  };
+  useEffect(() => {
+    setTimeout(
+      bottomChat.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      }),
+      100
+    );
+  }, [messages]);
+  const Topbar = ({ email }) => {
     return (
       <Flex bg="gray.100" h="81px" w="100%" align="center" p={5}>
         <Avatar src="" marginEnd={3} />
-        <Heading size="lg">user@gmail.com</Heading>
+        <Heading size="lg">{email}</Heading>
       </Flex>
     );
   };
 
-  const BottomBar = () => {
+  const BottomBar = ({ id }) => {
+    const [input, setInput] = useState(null);
+    const sendMessage = async (e) => {
+      e.preventDefault();
+      await addDoc(collection(db, "chats", id, "messages"), {
+        text: input,
+        sender: user.email,
+        timestamp: serverTimestamp(),
+      });
+      setInput("");
+    };
     return (
-      <Flex bg="blue.100" h="81px" p={3}>
-        <Input placeholder="Type a message" />
-        <Button type="submit" hidden autoComplete="off">
+      <FormControl p={3} onSubmit={sendMessage} as="form">
+        <Input
+          placeholder="Type a message"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button
+          type="submit"
+          hidden
+          autoComplete="off"
+          onClick={(e) => sendMessage(e)}
+        >
           Submit
         </Button>
-      </Flex>
+      </FormControl>
     );
   };
   return (
@@ -27,7 +111,7 @@ const chatBox = () => {
       <Flex h="100vh">
         <Sidebar />
         <Flex flex={1} direction="column">
-          <Topbar />
+          <Topbar email={getOtherEmail(chat?.users, user)} />
           <Flex
             flex={1}
             direction="column"
@@ -36,29 +120,10 @@ const chatBox = () => {
             overflowX="scroll"
             sx={{ scrollbarWidth: "none" }}
           >
-            <Flex
-              bg="blue.100"
-              w="fit-content"
-              minWidth="100px"
-              borderRadius="lg"
-              p={3}
-              m={1}
-            >
-              <Text>This is a dymmy message</Text>
-            </Flex>
-            <Flex
-              bg="green.100"
-              w="fit-content"
-              minWidth="100px"
-              borderRadius="lg"
-              p={3}
-              m={1}
-              alignSelf="flex-end"
-            >
-              <Text>This is a dymmy message</Text>
-            </Flex>
+            {getMessages()}
+            <div ref={bottomChat}></div>
           </Flex>
-          <BottomBar />
+          <BottomBar id={id} />
         </Flex>
       </Flex>
     </>
